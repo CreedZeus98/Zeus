9e8319957d608fabfa97df8ad228b34cf022d459
creedzeus98 <upendrasrv786@gmail.com>
2023-02-15 23:38:15 +0530

    8

---
+++ tri.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+speed('slowest')
+pencolor('black')
+fillcolor('blue')
+for i in range(3):
+    begin_fill()
+    fd(100)
+    lt(120)
+    end_fill()
+
+hideturtle()
+mainloop()
